38462d07a80517997f5637ffcd0a7099c73d88f7
Nathan Davidov <nathandavidov@Nathans-Laptop.lan>
2024-10-05 02:59:26 -0400

    add some documentation

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ const startPieces = [
 function createBoard(){
    
     startPieces.forEach((startPiece, i) => {
+        //Create a div for each piece on the starting board, call it "square"
         const square = document.createElement('div');
+        //set the innerHTML to our svg.
+        square.innerHTML = startPiece;
+        //set the id to be the index
         square.setAttribute('id', i);
+        //add 'square' and 'beige' to the class list of each square
         square.classList.add('square');
         square.classList.add('beige');
+        //finally, add the square to our gameBoard (which is itself a div)
         gameBoard.append(square);
       
     })
