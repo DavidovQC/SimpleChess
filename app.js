54068029e9a30add31e4c856da380b4c646d1b06
Nathan Davidov <nathandavidov@Nathans-Laptop.lan>
2024-10-06 02:29:58 -0400

    add dragability of pieces. Finish board creation. Add color to each square.

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const startPieces = [
 
 function createBoard(){
    
+    //start color of upper left hand side (i=0)
+    var color = 'beige';
     startPieces.forEach((startPiece, i) => {
         //Create a div for each piece on the starting board, call it "square"
         const square = document.createElement('div');
@@ -24,12 +26,36 @@ function createBoard(){
         square.setAttribute('id', i);
         //add 'square' and 'beige' to the class list of each square
         square.classList.add('square');
-        square.classList.add('beige');
+        
+        if(square.firstChild){
+            square.firstChild.setAttribute('draggable', true);
+        }
+
+        if((i+1) % 8 == 0){
+            square.classList.add(color);
+        }
+
+        else{
+            square.classList.add(color);
+            if(color == 'beige'){
+                color = 'brown';
+            }
+            else{
+                color = 'beige';
+            }
+        }
         //finally, add the square to our gameBoard (which is itself a div)
         gameBoard.append(square);
-      
+        if(i+1 <= 16){
+            square.firstChild.firstChild.classList.add('black');
+        }
+        else if(49 <= i+1){
+            square.firstChild.firstChild.classList.add('white');
+        }
     })
-    
 }
 
 createBoard();
+
+const squares = document.querySelectorAll('.square');
+console.log(squares);
